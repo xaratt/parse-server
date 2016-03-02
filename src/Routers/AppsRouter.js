3d2f2e7853d1f7f330a86d61d773431d09eb5331
Andry Gauk <xaratt@gmail.com>
2016-03-03 00:47:13 +0200

    start to add application management

--- src/Routers/AppsRouter.js
+++ src/Routers/AppsRouter.js
@@ -1,6 +1,6 @@
 import { Parse } from 'parse/node';
 import PromiseRouter from '../PromiseRouter';
-//import { AppsController } from '../Controllers/AppsController';
+import { AppsController } from '../Controllers/AppsController';
 
 function enforceMasterKeyAccess(req) {
   if (!req.auth.isMaster) {
@@ -11,39 +11,71 @@ function enforceMasterKeyAccess(req) {
 export class AppsRouter extends PromiseRouter {
 
   handleGet(req) {
-    var hooksController = req.config.hooksController;
-    if (req.params.functionName) {
-      return hooksController.getFunction(req.params.functionName).then( (foundFunction) => {
-        if (!foundFunction) {
-          throw new Parse.Error(143, `no function named: ${req.params.functionName} is defined`);
+    var appsController = req.config.appsController;
+    if (req.params.applicationId) {
+      return appsController.getApplication(req.params.applicationId, req.auth.config.masterKey).then( (foundApp) => {
+        if (!foundApp) {
+          throw new Parse.Error(143, `no app with id: ${req.params.applicationId} is defined`);
         }
-        return Promise.resolve({response: foundFunction});
+        return Promise.resolve({response: foundApp});
       });
     }
-    
-    return hooksController.getFunctions().then((functions) => {
-      return { response: functions || [] };
+
+    return appsController.getApplications().then((apps) => {
+      return { response: apps || [] };
     }, (err) => {
       throw err;
     });
   }
 
-  createApp(app, config) {
-    return config.hooksController.createHook(aHook).then( (hook) => ({response: hook}));
+  createApp(anApp, config) {
+    return config.appsController.createApp(anApp).then( (app) => ({response: app}));
   };
 
-  updateApp(aHook, config) {
-    return  config.hooksController.updateHook(aHook).then((hook) => ({response: hook}));
+  updateApp(anApp, config) {
+    return config.appsController.updateApp(anApp).then((app) => ({response: app}));
   };
 
   handlePost(req) {
     return this.createHook(req.body, req.config);
   };
 
+  createAppObjectFromRequest(applicationId, body) {
+    var app = {}
+    app.id = applicationId;
+    //TODO
+    // "appName": "<APPLICATION_NAME>",
+    // "applicationId": "<APPLICATION_ID>",
+    // "clientClassCreationEnabled": true,
+    // "clientPushEnabled": false,
+    // "dashboardURL": "https://www.parse.com/apps/yourapp",
+    // "javascriptKey": "<JAVASCRIPT_KEY>",
+    // "masterKey": "<MASTER_KEY>",
+    // "requireRevocableSessions": true,
+    // "restKey": "<REST_API_KEY>",
+    // "revokeSessionOnPasswordChange": true,
+    // "webhookKey": "<WEBHOOK_KEY>",
+    // "windowsKey": "<WINDOWS_KEY>"
+    // + user id
+    return app;
+  }
+
   handleUpdate(req) {
+    var app;
+    if (req.params.applicationId && req.body) {
+      app = createAppObjectFromRequest(req.params.applicationId && req.body);
+    } else {
+      throw new Parse.Error(143, "invalid application parameters");
+    }
+    return this.updateApp(app, req.config);
   };
 
   handleDelete(req) {
+    var appsController = req.config.appsController;
+    if (req.params.applicationId) {
+      return appsController.deleteApp(req.params.applicationId).then(() => ({response: {}}))
+    }
+    return Promise.resolve({response: {}});
   };
 
   handlePut(req) {
@@ -57,9 +89,10 @@ export class AppsRouter extends PromiseRouter {
 
   mountRoutes() {
     this.route('GET',  '/apps', enforceMasterKeyAccess, this.handleGet.bind(this));
+    this.route('GET',  '/apps/:applicationId', enforceMasterKeyAccess, this.handleGet.bind(this));
     this.route('POST', '/apps', enforceMasterKeyAccess, this.handlePost.bind(this));
-    this.route('PUT',  '/apps', enforceMasterKeyAccess, this.handlePut.bind(this));
-    this.route('DELETE',  '/apps', enforceMasterKeyAccess, this.handleDelete.bind(this));
+    this.route('PUT',  '/apps/:applicationId', enforceMasterKeyAccess, this.handlePut.bind(this));
+    this.route('DELETE',  '/apps/:applicationId', enforceMasterKeyAccess, this.handleDelete.bind(this));
   }
 }
 
